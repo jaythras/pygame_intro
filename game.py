900e757bbdb38275fe0e25ba758720dd8c6ce017
Jayden Thrasher <JaydenThrasher@gmail.com>
2019-09-27 17:15:21 -0400

    Add circle

--- game.py
+++ game.py
@@ -11,6 +11,9 @@ while not game_over:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       game_over = True
+
+    window.fill((255,255,255))
+    pygame.draw.circle(window, (0,0,255), (75,75), 75)
     pygame.display.flip()
 
 pygame.quit()
